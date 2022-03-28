a4964705b45b86191f539026652fba900aa62fab
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-29 00:22:45 +0900

    UPDATE(22.03.29)

--- Programmers/src/예산.java
+++ Programmers/src/예산.java
@@ -3,16 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        
         Arrays.sort(d);
         
         for(int i=0; i<d.length; i++){
-            if(d[i] <= budget){
+            
+            if(budget >= d[i]){
                 budget -= d[i];
                 answer++;
             } else {
                 break;
             }
         }
+        
         return answer;
     }
 }
