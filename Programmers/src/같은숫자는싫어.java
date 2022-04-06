088d34e9330196a74c5eb589d33137c609af1fa1
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-07 00:11:38 +0900

    UPDATE(22.04.07)

--- Programmers/src/같은숫자는싫어.java
+++ Programmers/src/같은숫자는싫어.java
@@ -19,3 +19,38 @@ public class Solution {
         return answer;
     }
 }
+
+//public class Solution {
+//    public int[] solution(int []arr) {
+//        List<Integer> list = new LinkedList<>();
+//
+//        int last = -1;
+//        for(int i=0; i<arr.length; i++){
+//            if(last == arr[i])  continue;
+//            last = arr[i];
+//            list.add(arr[i]);
+//        }
+//        
+//        int[] answer = new int[list.size()];
+//        
+//        for(int i=0; i<answer.length; i++) {
+//            answer[i] = list.get(i);
+//        }
+//        return answer;
+//    }
+//}
+
+//public class Solution {
+//    public int[] solution(int []arr) {
+//        List<Integer> list = new LinkedList<>();
+//
+//        int last = -1;
+//        for(int i=0; i<arr.length; i++){
+//            if(last == arr[i])  continue;
+//            last = arr[i];
+//            list.add(arr[i]);
+//        }
+//        
+//        return list.stream().mapToInt(Integer::intValue).toArray();
+//    }
+//}
