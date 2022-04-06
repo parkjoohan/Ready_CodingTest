3e451d998108fe91cd4830cad131675acf67bbc0
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-06 20:55:33 +0900

    UPDATE(22.04.06)

--- Programmers/src/완주하지못한선수.java
+++ Programmers/src/완주하지못한선수.java
@@ -1,16 +1,12 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion)     {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+    public String solution(String[] participant, String[] completion) {
+        List<String> list = new LinkedList<>();
         
-        int i;
-        for(i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
-            }
-        }
-        return participant[i];
+        for(String p : participant) list.add(p);
+        for(String c : completion)  list.remove(c);
+        
+        return list.get(0);
     }
 }
