6ad31521214d6b6deb22c5d47f77f242172f5a51
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-08 23:57:10 +0900

    UPDATE(22.04.08)

--- Programmers/src/문자열내마음대로정렬하기.java
+++ Programmers/src/문자열내마음대로정렬하기.java
@@ -17,3 +17,15 @@ class Solution {
         return strings;
     }
 }
+
+//class Solution {
+//    public String[] solution(String[] strings, int n) {
+//        
+//        Arrays.sort(strings, (s1, s2) -> {
+//            if(s1.charAt(n) == s2.charAt(n))    return s1.compareTo(s2);
+//            return s1.charAt(n) - s2.charAt(n);
+//        });
+//        
+//        return strings;
+//    }
+//}
