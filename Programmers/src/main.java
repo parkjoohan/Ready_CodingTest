a8a29d29536be7c5d057773f8f5628f98fcabc69
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-11 21:42:55 +0900

    UPDATE(22.04.11)

--- Programmers/src/main.java
+++ Programmers/src/main.java
@@ -5,12 +5,14 @@ class DuplicateWordsWithoutUsingMap {
         String duplicates = "";
         String[] words = str.toLowerCase().trim().split(" ");
         for (int i = 0; i < words.length; i++) {
+        	
             count = 1;
             for (int j = i + 1; j < words.length; j++) {
                 if (words[i].equals(words[j])) {
                     count++;
                 }
             }
+            
             if (count > 1) {
                 if (duplicates.isEmpty()) {
                     duplicates += " " + words[i] + " : " + count;
