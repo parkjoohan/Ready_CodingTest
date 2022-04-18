6bec3bbbd485a91f575bf5e4bad3d58192b8d78f
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-19 01:35:35 +0900

    UPDATE(22.04.19)

---
+++ Programmers/src/N개의최소공배수.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int solution(int[] arr) {
+        int num = lcm(arr[0], arr[1]);
+        int answer = 0;
+        
+        for(int i=2; i<arr.length; i++){
+            answer = lcm(num, arr[i]);
+            num = answer;
+        }
+        return answer;
+    }
+    
+    public int gcd(int a, int b) {
+        while(b!=0){
+            int r = a%b;
+            a=b;
+            b=r;
+        }
+        return a;
+    }
+    
+    public int lcm(int a, int b) {
+        return (a*b)/gcd(a,b);
+    }
+}
