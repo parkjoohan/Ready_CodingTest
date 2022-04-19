0f524da933cccf2c7cdd7304a76835f5be97be11
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-20 01:11:13 +0900

    UPDATE(22.04.20)

---
+++ Programmers/src/모음사전.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String word) {
+        int answer = word.length();
+        
+        String str = "AEIOU";
+        int[] x = {781, 156, 31, 6, 1};
+        
+        for(int i=0; i<word.length(); i++){
+            int idx = str.indexOf(word.charAt(i));
+            answer += x[i] * idx;
+            System.out.println(answer);
+        }
+        return answer;
+    }
+}
