d634f968962c5a62ae53aada335c58ae1654590c
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-22 21:59:02 +0900

    UPDATE(22.04.22)

---
+++ Programmers/src/멀쩡한사각형.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long solution(int w, int h) {
+        long x = w; 
+        long y = h;
+        long answer = x * y;
+        
+        long gcd = gcd(x, y);
+        
+        x /= gcd;
+        y /= gcd;
+        
+        answer = answer - gcd * (x+y-1);
+        
+        return answer;
+    }
+    
+    public long gcd(long x, long y){
+        long max = Math.max(x, y);
+        long min = Math.min(x, y);
+        
+        while(min != 0){
+            long temp = max % min;
+            max = min;
+            min = temp;
+        }
+        
+        return max;
+    }
+}
