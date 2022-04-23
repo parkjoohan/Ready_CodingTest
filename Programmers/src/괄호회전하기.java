35c50238cc9c98ac33d60a7fe7a4b757d4c1fc5d
alex0317@naver.com <joohanpark1513@gmail.com>
2022-04-24 01:08:22 +0900

    UPDATE(22.04.24)

---
+++ Programmers/src/괄호회전하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            Stack<String> st = new Stack<>();
+            s = s.substring(1, s.length())+s.substring(0,1);
+            
+            for(int j=0; j<s.length(); j++){
+                String word = s.substring(j, j+1);
+                if(st.isEmpty()){
+                    st.push(word);
+                    continue;
+                }
+                
+                if(word.equals(")") && st.peek().equals("(")){
+                    st.pop();
+                } else if(word.equals("}") && st.peek().equals("{")){
+                    st.pop();
+                } else if(word.equals("]") && st.peek().equals("[")){
+                    st.pop();
+                } else {
+                    st.push(word);
+                }
+            }
+            
+            if(st.isEmpty()){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
