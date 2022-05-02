d055d6dbbdee87d95c27eb24ab29e7c34fd3d88e
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 00:13:43 +0900

    UPDATE(22.05.03)

--- Programmers/src/예산.java
+++ Programmers/src/예산.java
@@ -19,3 +19,19 @@ class Solution {
         return answer;
     }
 }
+
+// javascript
+//function solution(d, budget) {
+//    var answer = 0;
+//    
+//    d.sort((a, b) => a-b);
+//    
+//    for(let i=0; i<d.length; i++){
+//        if(budget < d[i]) break;
+//        else {
+//            budget -= d[i];
+//            answer++;
+//        }
+//    }
+//    return answer;
+//}
