e77a3748c34584bcce2ec5db1b4eb8ac6fdabec3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 00:18:01 +0900

    UPDATE(22.05.03)

--- Programmers/src/내적.java
+++ Programmers/src/내적.java
@@ -9,3 +9,14 @@ class Solution {
         return answer;
     }
 }
+
+//javascript
+//function solution(a, b) {
+//    var answer = 0;
+//    
+//    for(let i=0; i<a.length; i++){
+//        answer += (a[i] * b[i]);
+//    }
+//    
+//    return answer;
+//}
