4e8bc5ce046e3c91fd00607612b70ebd8e9c595f
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 00:21:58 +0900

    UDPATE(22.05.03)

--- Programmers/src/두개뽑아서더하기.java
+++ Programmers/src/두개뽑아서더하기.java
@@ -25,3 +25,21 @@ class Solution {
         return answer;
     }
 }
+
+
+////javascript
+//function solution(numbers) {
+//    var answer = [];
+//    
+//    for(let i=0; i<numbers.length; i++){
+//        for(let j=i+1; j<numbers.length; j++){
+//            let sum = numbers[i] + numbers[j];
+//            if(answer.indexOf(sum) === -1){
+//                answer.push(sum);
+//            }
+//        }
+//    }
+//    answer.sort((a,b) => a-b);
+//    
+//    return answer;
+//}
