6c59076b487ee846e7cf6b6a85280f29e0e88871
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 00:05:06 +0900

    UPDATE(22.05.02)

--- Programmers/src/소수만들기.java
+++ Programmers/src/소수만들기.java
@@ -21,3 +21,26 @@ class Solution {
         return true;
     }
 }
+
+// javascript
+//function solution(nums) {
+//    var answer = 0;
+//    
+//    for(let i=0; i<nums.length-2; i++){
+//        for(let j=i+1; j<nums.length-1; j++){
+//            for(let k=j+1; k<nums.length; k++){
+//                if(isPrime(nums[i] + nums[j] + nums[k])) {
+//                  answer++;
+//                }
+//            }
+//        }
+//    }
+//    return answer;
+//}
+//
+//const isPrime = (num) => {
+//    for(let i=2; i<=Math.sqrt(num); i++){
+//        if(num % i == 0)    return false;
+//    }
+//    return true;
+//}
