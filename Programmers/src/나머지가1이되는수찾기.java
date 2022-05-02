6e1cb123474b6b594d727cac0d97f59f19886a6f
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 01:04:18 +0900

    UPDATE(22.05.03)

--- Programmers/src/나머지가1이되는수찾기.java
+++ Programmers/src/나머지가1이되는수찾기.java
@@ -12,3 +12,14 @@ class Solution {
         return answer;
     }
 }
+
+//// javascript
+//function solution(n) {
+//    var answer = 0;
+//    
+//    for(let i=2; i<n; i++){
+//        if(n%i===1) return i;
+//    }
+//    
+//    return answer;
+//}
