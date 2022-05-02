014f6efec9b59df2478a270a30be208f68d28604
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-03 00:41:13 +0900

    UPDATE(22.05.03)

--- Programmers/src/음양더하기.java
+++ Programmers/src/음양더하기.java
@@ -11,3 +11,17 @@ class Solution {
         return answer;
     }
 }
+
+//// javascript
+//function solution(absolutes, signs) {
+//    var answer = 0;
+//    
+//    for(let i=0; i<absolutes.length; i++){
+//        if(signs[i] == true)
+//            answer += absolutes[i];
+//        else
+//            answer -= absolutes[i];
+//    }
+//    
+//    return answer;
+//}
