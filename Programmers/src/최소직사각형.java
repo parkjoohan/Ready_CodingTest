ee3101c438a9e42147e5615a9be9d296f65723e4
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-31 21:14:53 +0900

    UPDATE(22.05.31)

--- Programmers/src/최소직사각형.java
+++ Programmers/src/최소직사각형.java
@@ -1,20 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] sizes) {
-        int max_width = 0;
-        int max_height = 0;
         int answer = 0;
+        int max_width=0;
+        int max_height=0;
         
         for(int i=0; i<sizes.length; i++){
             if(sizes[i][0]>=sizes[i][1]){
-                max_width = Math.max(sizes[i][0], max_width);
-                max_height = Math.max(sizes[i][1], max_height);
-            }else{
-                max_width = Math.max(sizes[i][1], max_width);
-                max_height = Math.max(sizes[i][0], max_height);
+                max_width=Math.max(sizes[i][0], max_width);
+                max_height=Math.max(sizes[i][1], max_height);
+            } else {
+                max_width=Math.max(sizes[i][1], max_width);
+                max_height=Math.max(sizes[i][0], max_height);
             }
         }
-        answer = max_width * max_height;
-        return answer;
+        
+        return max_width*max_height;
     }
 }
 
