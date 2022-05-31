2b6af93ebdd2754e58034f31f1cdbe7095138654
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-31 21:35:36 +0900

    UPDATE(22.05.31)

--- Programmers/src/부족한금액계산하기.java
+++ Programmers/src/부족한금액계산하기.java
@@ -1,14 +1,18 @@
 class Solution {
     public long solution(int price, int money, int count) {
-        long result = 0, sum = 0;
+        long answer = 0;
+        int sum = 0;
         
-        for(int i=1; i<=count; i++) {
+        for(int i=1; i<=count; i++){
             sum += price * i;
         }
-
-        result = sum - money;
-        if(result <= 0) return 0;
-        return result;
+        
+        answer = sum - money;
+        
+        if(answer <= 0)
+            return 0;
+        
+        return answer;
     }
 }
 
