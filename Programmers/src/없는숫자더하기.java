cacf1c77f80d485d203bbc2d7bbf43d498dea659
alex0317@naver.com <joohanpark1513@gmail.com>
2022-05-31 21:56:36 +0900

    UPDATE(22.05.31)

--- Programmers/src/없는숫자더하기.java
+++ Programmers/src/없는숫자더하기.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45; 
-        
-        for(int number : numbers) {
-            answer -= number;
+        int answer = 45;
+                
+        for(int i=0; i<numbers.length; i++){
+            answer -= numbers[i];
         }
         
         return answer;
