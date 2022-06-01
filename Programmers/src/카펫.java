90bdbf31a4bb4ae466c83f6d0cbec63d4a3f5db7
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-01 17:13:26 +0900

    UPDATE(22.06.01)

--- Programmers/src/카펫.java
+++ Programmers/src/카펫.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
         int area = brown + yellow;
         
         for(int i=1; i<=area; i++){
-            int row=i;
-            int col=area/row;
+            int row = i;
+            int col = area / row;
             
             // 카펫의 가로 길이는 >= 세로 길이
-            if(row>col) continue;
+            if(row>col)
+                continue;
             
             if((row-2)*(col-2)==yellow){
                 answer[0] = col;
