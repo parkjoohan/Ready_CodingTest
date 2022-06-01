819556804ae0bdc5036a0b139ab82e93d91ae4ae
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-01 17:04:38 +0900

    UPDATE(22.06.01)

--- Programmers/src/여행경로.java
+++ Programmers/src/여행경로.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
 class Solution {
-    
-    boolean[] visited;
-    ArrayList<String> answers;
-    
+    static boolean[] visit;
+    static ArrayList<String> answers;
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
+        visit = new boolean[tickets.length];
         answers = new ArrayList<>();
         int cnt = 0;
         
         dfs(cnt, "ICN", "ICN", tickets);
-        Collections.sort(answers);  // 답 중 알파벳순 정렬
+        // 답 중 알파벳순 정렬
+        Collections.sort(answers);
         
         // 가장 빠른 배열 뽑아서 String형 배열 전환
-        String[] answer = answers.get(0).split(" "); 
+        String[] answer = answers.get(0).split(" ");
         return answer;
     }
     
-    public void dfs(int cnt, String present, String answer, String[][] tickets){
+    public void dfs(int cnt, String present, String answer, String[][] tickets) {
         
         // 모든 공항 들른 뒤
         if(cnt == tickets.length){
@@ -27,13 +26,11 @@ class Solution {
         }
         
         for(int i=0; i<tickets.length; i++){
-            if(!visited[i] && tickets[i][0].equals(present)){
-                visited[i] = true;
+            if(!visit[i] && tickets[i][0].equals(present)){
+                visit[i] = true;
                 dfs(cnt+1, tickets[i][1], answer+" "+tickets[i][1], tickets);
-                visited[i] = false;
+                visit[i] = false;
             }
         }
-        
-        return;
     }
 }
