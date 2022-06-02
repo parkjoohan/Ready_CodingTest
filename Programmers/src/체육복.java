e0ca6782db5c2269c19bc7d7d0bb952721999c93
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-02 20:07:59 +0900

    UPDATE(22.06.02)

--- Programmers/src/체육복.java
+++ Programmers/src/체육복.java
@@ -1,31 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int count = 0;
+        int answer = n - lost.length;
+        
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        for(int i=0; i < lost.length; i++) {
-            for(int j=0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]) {
+        
+        // 여벌 체육복을 가져온 학생이 도난당한 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
                     lost[i] = -1;
-                    reserve[j] = -1;
-                    count++;
-                    break;
+                    reserve[j] = -1; 
+                    break; 
                 }
             }
         }
-        for (int i=0; i < lost.length; i++) {
-            for (int j=0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]+1 || lost[i] == reserve[j]-1) {
-                    count++;
-                    reserve[j] = -1;
-                    break;
-
+        // 도난당한 학생에게 체육복 빌려주는 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
+                    answer++;
+                    reserve[j] = -1; 
+                    break; 
                 }
             }
         }
-        return n - lost.length + count;
-
+        return answer;
     }
 }
