7c931d51f18746170f525c5751c8592fe4c3f27d
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 07:41:06 +0900

    UPDATE(22.06.09)

--- Programmers/src/두개뽑아서더하기.java
+++ Programmers/src/두개뽑아서더하기.java
@@ -3,19 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         ArrayList<Integer> list = new ArrayList<>();
-        
-        int sum=0;
+        int sum = 0;
         
         for(int i=0; i<numbers.length; i++){
             for(int j=i+1; j<numbers.length; j++){
                 sum = numbers[i] + numbers[j];
-                if(!list.contains(sum)){
+                if(!list.contains(sum))
                     list.add(sum);
-                }
             }
         }
         
         int[] answer = new int[list.size()];
+        
         for(int i=0; i<list.size(); i++){
             answer[i] = list.get(i);
         }
@@ -25,7 +24,6 @@ class Solution {
         return answer;
     }
 }
-
 
 ////javascript
 //function solution(numbers) {
