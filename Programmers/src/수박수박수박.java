56e25648adeb9c3a4c4c198bd5613fcdaaaa8705
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 07:45:17 +0900

    UPDATE(22.06.09)

--- Programmers/src/수박수박수박.java
+++ Programmers/src/수박수박수박.java
@@ -4,9 +4,9 @@ class Solution {
         
         for(int i=0; i<n; i++){
             if(i%2!=0)
-                answer += "박";
+                answer+="박";
             else
-                answer += "수";
+                answer+="수";
         }
         
         return answer;
