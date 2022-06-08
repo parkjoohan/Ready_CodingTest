c16409b50e736144e4f44eb8548f9c8d51a9e083
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 07:48:43 +0900

    UPDATE(22.06.09)

--- Programmers/src/두정수의합.java
+++ Programmers/src/두정수의합.java
@@ -2,14 +2,14 @@ class Solution {
     public long solution(int a, int b) {
         long answer = 0;
         
-        if(a<b) {
-            for(int i=a; i<=b; i++)
+        if(a<=b){
+            for(int i=a; i<b+1; i++)
                 answer += i;
-        } else if (a>b) {
-            for(int i=a; i>=b; i--)
+        } else {
+            for(int i=b; i<a+1; i++)
                 answer += i;
-        } else answer = a;
-      
+        }
+        
         return answer;
     }
 }
