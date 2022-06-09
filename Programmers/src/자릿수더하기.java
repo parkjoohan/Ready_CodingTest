cb0091240a7b0b6196522ff1899060e73b6be3bd
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 10:06:42 +0900

    UPDATE(22.06.09)

--- Programmers/src/자릿수더하기.java
+++ Programmers/src/자릿수더하기.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        
+
         while(n>0){
-            answer += (n%10);
-            n /= 10;
-        }   
-    
+            answer += n%10;
+            n = n/10;
+        }
+
         return answer;
     }
 }
