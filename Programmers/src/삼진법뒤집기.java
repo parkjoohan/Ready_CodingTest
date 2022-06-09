cb76147e7928e8e10e7a7e6013f22a2081f6f50e
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 16:46:49 +0900

    UPDATE(22.06.09)

--- Programmers/src/삼진법뒤집기.java
+++ Programmers/src/삼진법뒤집기.java
@@ -3,23 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+        String str = "";
         
-        // 10진법 -> 3진법
-        while(true) {
-            if(n<3){
-                list.add(n);
-                break;
-            }
-            list.add(n%3);
-            n=n/3;
+        while(n>0){
+            str += n%3;
+            n/=3;
         }
         
-        // 3진법 -> 10진법
-        for(int i=0; i<list.size(); i++){
-            answer += (Math.pow(3, list.size()-i-1)*list.get(i));    
-        }
-        
-        return answer;
+        return Integer.parseInt(str, 3);
     }
 }
