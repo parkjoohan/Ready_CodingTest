8d65ec812772c4fb669caaa3fce16aef6813133a
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 16:03:18 +0900

    UPDATE(22.06.09)

--- Programmers/src/최대공약수최소공배수.java
+++ Programmers/src/최대공약수최소공배수.java
@@ -1,3 +1,24 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int max = Math.max(n,m);
+        int min = Math.min(n,m);
+        
+        answer[0] = gcd(max, min);
+        answer[1] = max*min/answer[0];
+        
+        return answer;
+    }
+    
+    static int gcd(int x, int y){
+        if(x%y==0){
+            return y;
+        }
+        
+        return gcd(y, x%y);
+    }
+}
+
 // javascript
 //function solution(n, m) {
 //    var answer = [];
