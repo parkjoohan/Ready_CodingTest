e8a380322dbcec1122e23846d4cd5011860917ba
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 15:30:33 +0900

    UPDATE(22.06.09)

--- Programmers/src/약수의개수와덧셈.java
+++ Programmers/src/약수의개수와덧셈.java
@@ -2,17 +2,18 @@ class Solution {
     public int solution(int left, int right) {
         int answer = 0;
         
-        for(int i=left; i<=right; i++){
+        for(int i=left; i<right+1; i++){
             int cnt=0;
-            
             for(int j=1; j<=i; j++){
-                if(i%j==0)  cnt++;
+                if(i%j==0)
+                    cnt++;
             }
             
-            if(cnt%2==0)    answer+=i;
-            else    answer-=i;
+            if(cnt%2==0)
+                answer+=i;
+            else
+                answer-=i;
         }
-        
         return answer;
     }
 }
