d9450599c01c8f159aa10c7f6d4798a591179510
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 09:24:26 +0900

    UPDATE(22.06.09)

--- Programmers/src/이천십육년.java
+++ Programmers/src/이천십육년.java
@@ -1,17 +1,15 @@
 class Solution {
     public String solution(int a, int b) {
-        String answer = "";
-        String[] days = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
-        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int sum=0;
+        String[] day = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] days = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int sum=0, result=0;
         
-        for(int i=0; i<a-1; i++) {
-            sum += month[i];
+        for(int i=0; i<a-1; i++){
+            sum += days[i];
         }
-        
         sum += (b-1);
-        answer = days[sum%7];
+        result = sum%7;
         
-        return answer;
+        return day[result];
     }
 }
