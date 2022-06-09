66ef07f6119c686ecc8e2c1601ddd07532e1fbdb
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 16:14:12 +0900

    UPDATE(22.06.09)

--- Programmers/src/제일작은수제거하기.java
+++ Programmers/src/제일작은수제거하기.java
@@ -1,24 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-        if(arr.length <= 1){
+        
+        if(arr.length<=1){
             int[] answer = {-1};
             return answer;
         }
         
-        int min = arr[0];
-        for(int i=1; i<arr.length; i++){
-            min = Math.min(min, arr[i]);
-        }
-        
         int[] answer = new int[arr.length-1];
+        int min=Integer.MAX_VALUE;
         int cnt=0;
         
         for(int i=0; i<arr.length; i++){
-            if(arr[i] == min)
+            min = Math.min(min, arr[i]);
+        }
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==min)
                 continue;
-            answer[cnt++] = arr[i];
+            
+            answer[cnt++]=arr[i];
         }
         
         return answer;
