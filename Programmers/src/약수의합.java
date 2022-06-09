e5e33af00337170fe915caeb465221e51b2872c1
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 09:46:39 +0900

    UPDATE(22.06.09)

--- Programmers/src/약수의합.java
+++ Programmers/src/약수의합.java
@@ -1,11 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
         int answer = 0;
         
-        for(int i=1; i<=n; i++){
-            if(n%i==0){
-                answer += i;
-            }
+        for(int i=1; i<n+1; i++){
+            if(n%i==0)
+                list.add(i);
+        }
+        
+        for(int i=0; i<list.size(); i++){
+            answer += list.get(i);
         }
         
         return answer;
