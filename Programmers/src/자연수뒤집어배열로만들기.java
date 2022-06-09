0bb9cb477d7173f03429106f463f3f4d3c0bc15e
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-09 10:11:11 +0900

    UPDATE(22.06.09)

--- Programmers/src/자연수뒤집어배열로만들기.java
+++ Programmers/src/자연수뒤집어배열로만들기.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(long n) {
-        int[] answer = {};
         ArrayList<Integer> list = new ArrayList<>();
         
         while(n>0){
             list.add((int)(n%10));
-            n /= 10;
+            n = n/10;
         }
         
-        answer = new int[list.size()];
+        int[] answer = new int[list.size()];
         for(int i=0; i<list.size(); i++){
             answer[i] = list.get(i);
         }
