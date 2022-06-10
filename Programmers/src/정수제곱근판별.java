e23104307656a8b8d0d24b9142be5185edbd9841
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-10 22:33:00 +0900

    UPDATE(22.06.10)

--- Programmers/src/정수제곱근판별.java
+++ Programmers/src/정수제곱근판별.java
@@ -1,11 +1,12 @@
+import java.util.*;
+
 class Solution {
     public long solution(long n) {
         long num = (long)Math.sqrt(n);
         
-        if(Math.pow(num,2)==n){
-            return (long)Math.pow(num+1, 2);
-        } else {
+        if(Math.pow(num,2)==n)
+            return (long)Math.pow(num+1,2);
+        else
             return -1;
-        }
     }
 }
