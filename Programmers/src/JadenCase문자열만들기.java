13ae7d7e1fddcac15f72df8bb7c2eb32393099e3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-12 09:57:26 +0900

    UPDATE(22.06.12)

--- Programmers/src/JadenCase문자열만들기.java
+++ Programmers/src/JadenCase문자열만들기.java
@@ -1,16 +1,33 @@
-import java.util.*;
+//import java.util.*;
+//
+//class Solution {
+//    public String solution(String s) {
+//        String answer = "";
+//        
+//        String s2 = s.toLowerCase();
+//        char last = ' ';
+//        
+//        for(char c : s2.toCharArray()){
+//            if(last == ' ') c = Character.toUpperCase(c);
+//            answer += c;
+//            last = c;
+//        }
+//        
+//        return answer;
+//    }
+//}
 
 class Solution {
     public String solution(String s) {
         String answer = "";
+        String[] arr = s.toLowerCase().split("");
+        StringBuilder sb = new StringBuilder();
         
-        String s2 = s.toLowerCase();
-        char last = ' ';
+        boolean blank = true;
         
-        for(char c : s2.toCharArray()){
-            if(last == ' ') c = Character.toUpperCase(c);
-            answer += c;
-            last = c;
+        for(String i : arr){
+            answer += blank ? i.toUpperCase() : i;
+            blank = i.equals(" ") ? true : false;
         }
         
         return answer;
