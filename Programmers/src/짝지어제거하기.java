3d30c62b4d0674feefbcb01daef7009c0ae1b6ca
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-12 09:06:38 +0900

    UPDATE(22.06.12)

--- Programmers/src/짝지어제거하기.java
+++ Programmers/src/짝지어제거하기.java
@@ -4,12 +4,12 @@ class Solution
 {
     public int solution(String s)
     {
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             
-            if(!stack.isEmpty() && ch == stack.peek())
+            if(!stack.isEmpty() && ch==stack.peek())
                 stack.pop();
             else
                 stack.add(ch);
