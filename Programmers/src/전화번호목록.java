b386298ccd7ffe57f3a9cb837f3c49216de2d667
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-13 08:09:54 +0900

    UPDATE(22.06.13)

--- Programmers/src/전화번호목록.java
+++ Programmers/src/전화번호목록.java
@@ -6,10 +6,10 @@ class Solution {
         
         Arrays.sort(phone_book);
         for(int i=0; i<phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
+            if(phone_book[i+1].startsWith(phone_book[i]))
                 return false;
-            }
-        }
+        }    
+    
         return answer;
     }
 }
