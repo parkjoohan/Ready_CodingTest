84c75b9c14ec3bc41d26ea2301965117e6f08539
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-13 10:02:36 +0900

    UPDATE(22.06.13)

--- Programmers/src/게임맵최단거리.java
+++ Programmers/src/게임맵최단거리.java
@@ -1,18 +1,26 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int[][] visited;
     public int solution(int[][] maps) {
         int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
+        visited = new int[maps.length][maps[0].length];
         
+        // 시작 위치 방문 체크
         visited[0][0]=1;
         
+        // bfs 탐색
         bfs(maps, visited);
+        
+        // 도착지 값
         answer = visited[maps.length-1][maps[0].length-1];
         
-        if(answer == 0) answer = -1;
+        // 끝까지 못 가는 경우
+        if(answer == 0)
+            answer = -1;
         
         return answer;
     }
@@ -31,10 +39,10 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                // 범위 벗어났거나 방문했는지, 갈 수 있는지 체크
-                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && visited[nx][ny]==0 & maps[nx][ny] == 1){
-                    // 방문 체크하기
-                    visited[nx][ny] = visited[x][y] + 1;
+                // 범위 벗어나는지, 방문했는지, 갈 수 있는지 확인
+                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length && visited[nx][ny]==0 && maps[nx][ny]==1){
+                    // 방문 처리하기
+                    visited[nx][ny] = visited[x][y]+1;
                     q.add(new int[]{nx, ny});
                 }
             }
