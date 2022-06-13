4e95f03e18c420d376a7fe9fe8a44b1cc4ce66b9
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-14 00:48:38 +0900

    UPDATE(22.06.14)

--- Programmers/src/피보나치수.java
+++ Programmers/src/피보나치수.java
@@ -1,19 +1,19 @@
 class Solution {
- 
     public int solution(int n) {
         int answer = 0;
-        int num1 = 1;
-        int num2 = 1;
+        int num1=1;
+        int num2=1;
         
-        if(n==1 || n==2) return 1;
+        if(n==1 || n==2)
+            return -1;
         else {
-            for(int i=3; i<=n; i++) {
+            for(int i=3; i<=n; i++){
                 answer = (num1+num2)%1234567;
-                num1 = num2;
-                num2 = answer;
-                
+                num1=num2;
+                num2=answer;
             }
-            return answer;
         }
+        
+        return answer;
     }
 }
