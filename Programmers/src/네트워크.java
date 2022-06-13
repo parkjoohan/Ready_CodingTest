26de07ac04d5336cde78ca91d893bd0b82b4a772
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-13 22:06:09 +0900

    UPDATE(22.06.13)

--- Programmers/src/네트워크.java
+++ Programmers/src/네트워크.java
@@ -1,24 +1,30 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] check = new boolean[n];
+        // 방문 여부 저장 
+        boolean[] visited = new boolean[n];
         
-        for(int i=0; i<n; i++){
-            if(!check[i]){
-                dfs(computers, check, i);
+        for(int i=0; i<computers.length; i++){
+            // 해당 컴퓨터가 방문된 적이 없으면
+            if(!visited[i]){
+                // 해당 컴퓨터를 시작으로 방문 실행
+                dfs(computers, visited, i);
+                // 네트워크 하나가 탐색되었기 떄문에 카운트 +1
                 answer++;
             }
         }
         return answer;
     }
     
-    public void dfs(int[][] computers, boolean[] check, int node){
-        check[node] = true;
+    // 컴퓨터 연결정보 행렬, 컴퓨터 방문 여부, 시작 컴퓨터 
+    void dfs(int[][] computers, boolean[] visited, int start){
+        // 시작 컴퓨터 방문
+        visited[start] = true;
         
-        for(int i=0; i<computers.length; i++){
-            if(!check[i] && computers[node][i]==1){
-                dfs(computers, check, i);
-            }
+        for(int i=0; i<computers[start].length; i++){
+            // 시작 컴퓨터와 연결되어 있는 컴퓨터들을 새 시작저믕로 방문 명령
+            if(computers[start][i]==1 && visited[i]==false)
+                dfs(computers, visited, i);
         }
     }
 }
