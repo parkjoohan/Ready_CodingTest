7d171825c70e365671fcad72e04d854765cb7af6
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-13 09:09:18 +0900

    UPDATE(22.06.13

--- Programmers/src/가장큰수.java
+++ Programmers/src/가장큰수.java
@@ -2,27 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        String[] result = new String[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            result[i] = String.valueOf(numbers[i]);
-        }
-        
-        Arrays.sort(result, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2){
-                return (o2+o1).compareTo(o1+o2);
-            }
-        });
-        
-        if(result[0].equals("0"))
-            return "0";
-        
         String answer = "";
         
-        for(String str : result){
+        // int -> String으로 바꿔서 저장
+        ArrayList<String> list = new ArrayList<>();
+        for(int num : numbers)
+            list.add(String.valueOf(num));
+        
+        // 내림차순 정렬
+        Collections.sort(list, (a, b) -> (b+a).compareTo(a+b));
+        
+        // 0이 앞에 나올 경우 return 0
+        if(list.get(0).startsWith("0"))
+            return "0";
+        
+        // answer에 담기
+        for(String str : list)
             answer += str;
-        }
         
         return answer;
     }
