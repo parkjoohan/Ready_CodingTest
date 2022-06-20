fa287b81c7179c876fb770e8b046c504205268fd
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-20 12:01:06 +0900

    UPDATE(22.06.20)

--- Programmers/src/로또의최고순위와최저순위.java
+++ Programmers/src/로또의최고순위와최저순위.java
@@ -2,23 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int cnt = 0; //당첨번호와 비교해 일치하는 번호 개수 cnt
-        int zeroCnt = 0; //동생 낙서로 알아보지 못하는 번호 개수 zeroCnt
-    
-    for(int lotto: lottos) { //민우의 로또번호 반복문으로 하나씩 돌리기
-        if(lotto == 0) { //로또번호가 0이면 
-            zeroCnt ++; //zeroCnt 1씩 증가시키기
-            continue; //바로 다음 로또번호 검색하도록 continue
-        } 
-        //만약 로또번호가 0이 아니면
-        for(int win_num: win_nums) { //당첨번호와 민우 로또번호를 비교해 
-            if(win_num == lotto){ //서로 일치하면
-                cnt ++; //cnt를 1씩 증가시켜줌
-                break; //다음 번호를 비교하기 위해 빠져나옴
+        int[] answer = new int[2];
+        int match=0;
+        int not_match=0;
+        
+        Arrays.sort(lottos);
+        Arrays.sort(win_nums);
+        
+        // 알아보지 못하는 번호
+        for(int i=0; i<lottos.length; i++){
+            if(lottos[i]==0){
+                not_match++;
+                continue;
+            }
+            
+            for(int j=0; j<win_nums.length; j++){
+                if(lottos[i]==win_nums[j]){
+                    match++;
+                    break;
+                }
             }
         }
+        
+        // 알아보지 못하는 번호, 모두 당첨 번호와 일치
+        answer[0] = get(match+not_match);
+        // 알아보지 못하는 번호, 모두 당첨 번호와 불일치
+        answer[1] = get(match);
+        
+        return answer;
     }
-    return new int[]{7-Math.max(cnt+zeroCnt,1),7-Math.max(cnt,1)};
     
-}
+    public static int get(int n){
+        switch(n){
+            case 6: return 1;
+            case 5: return 2;
+            case 4: return 3;
+            case 3: return 4;
+            case 2: return 5;
+            default: return 6;
+        }
+    }
 }
