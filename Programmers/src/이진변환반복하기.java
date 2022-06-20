934676252974e63b107dce871d9d1b0084be01f9
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-20 20:44:01 +0900

    UPDATE(22.06.20)

--- Programmers/src/이진변환반복하기.java
+++ Programmers/src/이진변환반복하기.java
@@ -3,13 +3,19 @@ class Solution {
         int[] answer = new int[2];
         
         while(s.length()>1){
-            int cnt = 0;
+            // 문자열 길이
+            int cnt=0;
             
             for(int i=0; i<s.length(); i++){
-                if(s.charAt(i) == '0')  answer[1]++;
-                else    cnt++;
+                // 0이 있으면 제거한 개수 +
+                if(s.charAt(i)=='0')
+                    answer[1]++;
+                // 0이 아니면 그냥 문자열 길이 +
+                else
+                    cnt++;
             }
             
+            // 문자열 길이를 이진으로 바꾸기
             s = Integer.toBinaryString(cnt);
             answer[0]++;
         }
