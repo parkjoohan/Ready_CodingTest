462fda383e221246fbb46cbd76bf7d140e32233f
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-30 21:54:02 +0900

    UPDATE(22.06.30)

---
+++ Programmers/src/코테준비/1_정수내림차순으로배치하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(n>0){
+            list.add((int)n%10);
+            n/=10;
+        }
+        
+        Collections.sort(list);
+        
+        for(int i=0; i<list.size(); i++)
+            answer+=list.get(i)*Math.pow(10,i);
+        
+        return answer;
+    }
+}
