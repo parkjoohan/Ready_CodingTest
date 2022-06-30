3f45109e1e590711112e9be257e8f5190d6189c3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-30 10:47:42 +0900

    UPDATE(22.06.30)

---
+++ Programmers/src/코테준비/1_없는숫자더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int num : numbers)
+            answer -= num;
+        
+        return answer;
+    }
+}
