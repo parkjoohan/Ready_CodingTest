7fa184e2ef6be25b86d32807f9f880b67061ee67
alex0317@naver.com <joohanpark1513@gmail.com>
2022-06-30 23:34:57 +0900

    UPDATE(22.06.30)

---
+++ Programmers/src/코테준비/1_정수제곱근판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long num = (long)Math.sqrt(n);
+        
+        if(Math.pow(num,2)==n)
+            return (long)Math.pow(num+1,2);
+        else
+            return -1;
+    }
+}
