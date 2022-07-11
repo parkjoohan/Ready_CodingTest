20a998f108b9f1bc45dc1a3a0a5c291bd7212aff
alex0317@naver.com <joohanpark1513@gmail.com>
2022-07-11 23:11:40 +0900

    코테 준비

---
+++ Programmers/src/Javascript/공던지기.js
@@ -0,0 +1,12 @@
+function solution(numbers, k) {
+    let answer = 0;
+    
+    for(let i=0; i<k; i++){
+        answer += 2;
+        
+        if(answer > numbers.length) 
+            answer -= numbers.length;
+    }
+    
+    return numbers[answer-2];
+}
