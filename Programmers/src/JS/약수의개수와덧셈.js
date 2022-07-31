7ea0647f3ca5500396b52f006f4b39c16fbcdd1d
alex0317@naver.com <joohanpark1513@gmail.com>
2022-07-31 10:32:11 +0900

    UPDATE(22.07.31)

---
+++ Programmers/src/JS/약수의개수와덧셈.js
@@ -0,0 +1,16 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for(let i=left; i<=right; i++){
+        let arr = [];
+        
+        for(let j=0; j<=i; j++){
+            if(i%j==0)
+                arr.push(j);
+        }
+        
+        arr.length%2 === 0 ? answer+=i : answer-= i;
+    }
+    
+    return answer;
+}
