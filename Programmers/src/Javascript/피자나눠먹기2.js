4940eec58d1268e202fc3c0a3f3ac61f30d9f22a
alex0317@naver.com <joohanpark1513@gmail.com>
2022-09-22 21:13:54 +0900

    코테 준비

---
+++ Programmers/src/Javascript/피자나눠먹기2.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let num = 1;
+    
+    while(true) {
+        if((num*6)%n==0){
+            return num;
+            break;
+        } else 
+            num++;
+    }
+    
+    return answer;
+}
