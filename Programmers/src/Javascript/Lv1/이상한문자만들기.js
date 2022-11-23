e8e338b1c20ef87be9d626791e927d1a8b15d2fe
alex0317@naver.com <joohanpark1513@gmail.com>
2022-11-23 11:42:42 +0900

    코테 준비

--- Programmers/src/Javascript/Lv1/이상한문자만들기.js
+++ Programmers/src/Javascript/Lv1/이상한문자만들기.js
@@ -1,3 +1,22 @@
+// function solution(s) {
+//     return s.split(" ").map((x) => x.split("").map((y,index) => (index%2==0 ? y.toUpperCase() : y.toLowerCase())).join("")).join(" ");
+// }
+
 function solution(s) {
-    return s.split(" ").map((x) => x.split("").map((y,index) => (index%2==0 ? y.toUpperCase() : y.toLowerCase())).join("")).join(" ");
+    var answer = '';
+    let words = s.split(" ");
+    
+    for(let i=0; i<words.length; i++){
+        for(let j=0; j<words[i].length; j++){
+            if(j%2 === 0)
+                answer += words[i][j].toUpperCase();
+            else
+                answer += words[i][j].toLowerCase();
+        }
+        
+        if(i<words.length-1)
+            answer += " ";
+    }
+    
+    return answer;
 }
