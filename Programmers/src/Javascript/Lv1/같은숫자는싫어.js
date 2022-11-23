83bcee0ce2f8218f1faf15bbc9d6bd19745d4d86
alex0317@naver.com <joohanpark1513@gmail.com>
2022-11-23 11:34:22 +0900

    코테 준비

--- Programmers/src/Javascript/Lv1/같은숫자는싫어.js
+++ Programmers/src/Javascript/Lv1/같은숫자는싫어.js
@@ -1,10 +1,15 @@
+// function solution(arr)
+// {
+//     var answer = [];
+
+//     for(let i=0; i<arr.length; i++)
+//         if(arr[i]!==arr[i+1])
+//             answer.push(arr[i]);
+    
+//     return answer;
+// }
+
 function solution(arr)
 {
-    var answer = [];
-
-    for(let i=0; i<arr.length; i++)
-        if(arr[i]!==arr[i+1])
-            answer.push(arr[i]);
-    
-    return answer;
+    return arr.filter((val,index) => val != arr[index+1]);
 }
