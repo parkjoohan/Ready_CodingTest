56ba4eea0f5622e36c82cab8137a25d12d9b33a4
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-07 07:58:48 +0900

    코테 준비

--- Programmers/src/Javascript/Lv2/올바른괄호.js
+++ Programmers/src/Javascript/Lv2/올바른괄호.js
@@ -1,16 +1,29 @@
+// function solution(s){
+//     let stack = [], cnt=0;
+    
+//     for(let i=0; i<s.length; i++) {
+//         if(s[i]==="(") {
+//             stack.push("(");
+//             cnt++;
+//         } else {
+//             stack.pop();
+//             cnt--;
+//         }
+//     }
+    
+//     if(stack.length>0 || cnt!==0)   return false;
+//     return true;
+// }
+
 function solution(s){
-    let stack = [], cnt=0;
-    
-    for(let i=0; i<s.length; i++) {
-        if(s[i]==="(") {
-            stack.push("(");
-            cnt++;
-        } else {
-            stack.pop();
-            cnt--;
-        }
+    for(let i =0, cnt =0;i < s.length;i++){
+        if (s[i] == "(")    
+            cnt +=1;
+        if (s[i] == ")")   
+            cnt -= 1;
+        if (cnt < 0 )   
+            return false
+        if ( i == s.length -1 )    
+            return cnt == 0 ? true : false
     }
-    
-    if(stack.length>0 || cnt!==0)   return false;
-    return true;
 }
