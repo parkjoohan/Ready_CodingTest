71cac74f2f8ed86d4da1cffb3f47b8d5fc0a3303
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-06 11:37:00 +0900

    코테 준비

---
+++ Programmers/src/Javascript/Lv1/옹알이2.js
@@ -0,0 +1,19 @@
+function solution(babbling) {
+    let answer = 0;
+
+    babbling.forEach((word) => {
+        word = word.replaceAll("ayaaya", "*");
+        word = word.replaceAll("yeye", "*");
+        word = word.replaceAll("woowoo", "*");
+        word = word.replaceAll("mama", "*");
+
+        word = word.replaceAll("aya", " ");
+        word = word.replaceAll("ye", " ");
+        word = word.replaceAll("woo", " ");
+        word = word.replaceAll("ma", " ");
+
+        if (word.replaceAll(" ", "").length === 0) answer++;
+    });
+
+    return answer;
+}
