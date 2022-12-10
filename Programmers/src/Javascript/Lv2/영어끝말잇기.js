43b3efa9849133c27d0c6e6e1715a481799aee38
alex0317@naver.com <joohanpark1513@gmail.com>
2022-12-10 21:30:10 +0900

    코테 준비

---
+++ Programmers/src/Javascript/Lv2/영어끝말잇기.js
@@ -0,0 +1,14 @@
+function solution(n, words) {
+    let array = [];
+    
+    for(let i=0; i<words.length; i++) {
+        if(i===0)   array.push(words[i]);
+        else if(!array.includes(words[i]) && words[i-1][words[i-1].length-1] === words[i][0])
+            array.push(words[i]);
+        else {
+            return [i%n+1, parseInt(i/n)+1];
+        }
+    }
+    
+    return [0,0];
+}
---
+++ Programmers/src/Javascript/Lv2/짝지어제거하기.js
@@ -0,0 +1,15 @@
+function solution(s)
+{
+    let answer = 0;
+    let stack = [];
+    
+    for(let i=0; i<s.length; i++){
+        if(stack[stack.length-1] === s.charAt(i))
+            stack.pop();
+        else
+            stack.push(s.charAt(i));
+    }
+
+    answer = stack.length === 0 ? 1 : 0;
+    return answer;
+}
