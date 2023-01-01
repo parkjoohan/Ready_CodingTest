8cabaafe965c31872d78b6c6829caee9738d57ac
alex0317@naver.com <joohanpark1513@gmail.com>
2023-01-02 01:49:12 +0900

    코테 준비

---
+++ Programmers/src/Javascript/Lv2/프린터.js
@@ -0,0 +1,25 @@
+function solution(priorities, location) {
+    var answer = 0;
+    let cnt = 0;
+    let curDoc = location;
+    
+    while(priorities.length > 0) {
+        let current = priorities.shift();
+        
+        if(priorities.filter((e) => e>current).length > 0) {
+            priorities.push(current);
+        } else {
+            cnt++;
+            
+            if(curDoc == 0)
+                return answer =cnt;
+        }
+        
+        curDoc--;
+        
+        if(curDoc === -1) 
+            curDoc = priorities.length-1;
+    }
+    
+    return answer;
+}
