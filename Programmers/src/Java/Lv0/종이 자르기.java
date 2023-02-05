746cc2383f9c9e587885b4a92a0ba3cc4effbcc4
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-06 03:22:24 +0900

    코테 준비

---
+++ Programmers/src/Java/Lv0/종이 자르기.java
@@ -0,0 +1,11 @@
+package Java.Lv0;
+
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        answer = (M - 1) + (M * (N - 1));
+        
+        return answer;
+    }
+}
