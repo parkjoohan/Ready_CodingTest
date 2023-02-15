53ad65e703ea608b0eaed0fbb3c4d78f7d7c6298
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-15 23:45:09 +0900

    코테 준비

---
+++ Programmers/src/Java/Lv1/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(long n) {
+        String s = ""+n;
+        int[] answer = new int[s.length()];
+        
+        for(int i=0; i<s.length(); i++) {
+            answer[i] = (int)(n%10);
+            n/=10;
+        }
+        
+        return answer;
+    }
+}
