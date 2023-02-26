e94d5eb4e8a34ef3f766dd217e45384b19b8879c
alex0317@naver.com <joohanpark1513@gmail.com>
2023-02-26 23:34:09 +0900

    코테 준비

---
+++ Programmers/src/Java/Lv1/두 개 뽑아서 더하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<numbers.length-1; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                
+                if(!list.contains(sum))
+                    list.add(sum);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++)
+            answer[i] = list.get(i);
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
